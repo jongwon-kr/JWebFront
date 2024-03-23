b72df8f7c8a4d502072f53b529852b871fa94c53
jongwon-kr <growjong8802@gmail.com>
2024-03-23 23:40:02 +0900

    finish section 10

--- todo-app/src/components/counter/CounterButton.jsx
+++ todo-app/src/components/counter/CounterButton.jsx
@@ -1,26 +1,13 @@
-import { useState } from "react";
 import PropTypes from "prop-types";
 
 export function CounterButton({ by, incrementMethod, decrementMethod }) {
-  const [count, setCount] = useState(0);
-
-  function incrementCounter() {
-    setCount(count + by);
-    incrementMethod(by);
-  }
-
-  function decrementCounter() {
-    setCount(count - by);
-    decrementMethod(by);
-  }
-
   return (
     <div className="Counter">
       <div>
-        <button className="counterButton" onClick={incrementCounter}>
+        <button className="counterButton" onClick={() => incrementMethod(by)}>
           +{by}
         </button>
-        <button className="counterButton" onClick={decrementCounter}>
+        <button className="counterButton" onClick={() => decrementMethod(by)}>
           -{by}
         </button>
       </div>
