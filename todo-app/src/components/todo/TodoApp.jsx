afec1907d671ce110160bbc5636887cf6ee1b482
jongwon-kr <growjong8802@gmail.com>
2024-03-25 00:07:05 +0900

    use Link to

--- todo-app/src/components/todo/TodoApp.jsx
+++ todo-app/src/components/todo/TodoApp.jsx
@@ -1,10 +1,26 @@
 import { useState } from "react";
 import "./TodoApp.css";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useNavigate,
+  useParams,
+  Link,
+} from "react-router-dom";
 
 export default function TodoApp() {
   return (
     <div className="App">
-      <LoginComponent></LoginComponent>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginComponent />} />
+          <Route path="/login" element={<LoginComponent />} />
+          <Route path="/welcome/:username" element={<WelcomeComponent />} />
+          <Route path="*" element={<ErrorComponent />} />
+          <Route path="/todos" element={<ListTodosComponent />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
@@ -17,6 +33,8 @@ function LoginComponent() {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
   const [showErrorMessage, setshowErrorMessage] = useState(false);
 
+  const navigate = useNavigate();
+
   function handleUsernameChange(event) {
     setUsername(event.target.value);
   }
@@ -27,38 +45,28 @@ function LoginComponent() {
 
   function handleSubmit() {
     if (username === "jongwon" && password === "1234") {
+      console.log("Success");
       setShowSuccessMessage(true);
       setshowErrorMessage(false);
+      navigate(`/welcome/${username}`);
     } else {
+      console.log("Failed");
       setShowSuccessMessage(false);
       setshowErrorMessage(true);
     }
   }
 
-  function SuccessMessageComponent() {
-    if (showSuccessMessage) {
-      return <div className="successMessage">Authenticated Successfully</div>;
-    }
-
-    return null;
-  }
-
-  function ErrorMessageComponent() {
-    if (showErrorMessage) {
-      return (
+  return (
+    <div className="Login">
+      <h1>To Login!</h1>
+      {showSuccessMessage && (
+        <div className="successMessage">Authenticated Successfully</div>
+      )}
+      {showErrorMessage && (
         <div className="errorMEssage">
           Authentication Failed. Please check your credentials.
         </div>
-      );
-    }
-
-    return null;
-  }
-
-  return (
-    <div className="Login">
-      <SuccessMessageComponent></SuccessMessageComponent>
-      <ErrorMessageComponent></ErrorMessageComponent>
+      )}
       <div className="LoginForm">
         <div className="label">
           <label>User Namne:</label>
@@ -89,5 +97,65 @@ function LoginComponent() {
 }
 
 function WelcomeComponent() {
-  return <div>Welcome Component</div>;
+  const { username } = useParams();
+  return (
+    <div className="WelcomeComponent">
+      <h1>Welcome {username}</h1>
+      <div>
+        Manage Your todos. <Link to="/todos">Go here</Link>
+      </div>
+    </div>
+  );
+}
+
+function ErrorComponent() {
+  return (
+    <div className="ErrorComponent">
+      <h1>We are working really hard!</h1>
+      <div>Apologies for the 404. Reach out to our team at 010-0000-0000</div>
+    </div>
+  );
+}
+
+function ListTodosComponent() {
+  const today = new Date();
+  const targetDate = new Date();
+
+  const todos = [
+    { id: 1, description: "Learn AWS", done: false, targetDate: targetDate },
+    {
+      id: 2,
+      description: "Learn SpringBoot",
+      done: false,
+      targetDate: targetDate,
+    },
+    { id: 3, description: "Learn React", done: false, targetDate: targetDate },
+  ];
+  return (
+    <div className="ListTodosComponent">
+      <h1>To do List!</h1>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <td>Id</td>
+              <td>Description</td>
+              <td>Is Done?</td>
+              <td>Target Date</td>
+            </tr>
+          </thead>
+          <tbody>
+            {todos.map((todo) => (
+              <tr key={todo.id}>
+                <td>{todo.id}</td>
+                <td>{todo.description}</td>
+                <td>{todo.done.toString()}</td>
+                <td>{todo.targetDate.toDateString()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
