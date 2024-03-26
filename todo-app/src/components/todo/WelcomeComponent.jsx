1a40023520736006517bb0142721eae35a59a308
jongwon-kr <growjong8802@gmail.com>
2024-03-26 16:10:31 +0900

    restapi - ReactWebApp

--- todo-app/src/components/todo/WelcomeComponent.jsx
+++ todo-app/src/components/todo/WelcomeComponent.jsx
@@ -1,13 +1,47 @@
 import { useParams, Link } from "react-router-dom";
+import { useState } from "react";
+import { retrieveHelloWorldBean } from "./api/HelloWorldApiService";
 
 export function WelcomeComponent() {
   const { username } = useParams();
+
+  const [message, setMessage] = useState(null);
+
+  function callHelloWorldRestApi() {
+    console.log("called");
+
+    // axios
+    //   .get("http://localhost:8080/hello-world")
+    //   .then((response) => successfulResponse(response))
+    //   .catch((error) => errorResponse(error))
+    //   .finally(() => console.log("cleanup"));
+    retrieveHelloWorldBean()
+      .then((response) => successfulResponse(response))
+      .catch((error) => errorResponse(error))
+      .finally(() => console.log("cleanup"));
+  }
+
+  function successfulResponse(response) {
+    console.log(response);
+    // setMessage(response.data);
+    setMessage(response.data.message);
+  }
+
+  function errorResponse(response) {
+    console.log(response);
+  }
   return (
     <div className="WelcomeComponent">
       <h1>Welcome {username}</h1>
       <div>
         Manage Your todos. <Link to="/todos">Go here</Link>
       </div>
+      <div>
+        <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>
+          Call Hello World REST API
+        </button>
+      </div>
+      <div className="test-info">{message}</div>
     </div>
   );
 }
